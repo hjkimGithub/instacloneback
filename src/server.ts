cd3fee7564399b897cf5371c79d763b62c309350
김현재 <rlaguswo3408@gmail.com>
2023-01-10 10:55:30 +0900

    auth, resolver 은닉(포장...?) ~4.13

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 require('dotenv').config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
+import { getUser } from "./users/users.utils";
 
 const server = new ApolloServer({
   schema,
-  context: {
-    token:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjczMjQzNDg0fQ.0LanWZ5ozcfN2a088Kik7HncNtIFqWCGf2heHauveeI"
-  },
+  context: async ({req}) => {
+    // console.log(req.headers)
+    return {
+      // token: req.headers.token
+      // token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjczMjQzNDg0fQ.0LanWZ5ozcfN2a088Kik7HncNtIFqWCGf2heHauveeI"
+      loggedInUser: await getUser(req.headers.token),
+    };
+  }
 });
 
 const PORT = process.env.PORT;
--- src/users/editProfile/editProfile.resolvers.ts
+++ src/users/editProfile/editProfile.resolvers.ts
@@ -1,48 +1,57 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword },
+  {loggedInUser, protectedResolver}
+) => {
+  // console.log(loggedInUser)
+  // if(!loggedInUser) {
+  //   throw new Error("You need to login.");
+  // }
+  // protectedResolver(loggedInUser);
+  // const verifiedToken = await jwt.verify(token, process.env.SECRET_KEY);  
+  // console.log(verifiedToken);    
+  // {
+  //    id: ~~
+  //    iat: ~~~
+  // }
+  let uglyPassword = null;
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      ...(uglyPassword && { password: uglyPassword }),
+    },
+  });
+  if (updatedUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "Could not update profile.",
+    };
+  }
+}
 
 const resolvers = {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword },
-      {token}
-    ) => {
-      // const verifiedToken = await jwt.verify(token, process.env.SECRET_KEY);  
-      // console.log(verifiedToken);    
-      // {
-      //    id: ~~
-      //    iat: ~~~
-      // }
-      const {id}:any = await jwt.verify(token, process.env.SECRET_KEY);
-      let uglyPassword = null;
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
-      }
-      const updatedUser = await client.user.update({
-        where: {
-          id,
-        },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(uglyPassword && { password: uglyPassword }),
-        },
-      });
-      if (updatedUser.id) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "Could not update profile.",
-        };
-      }
-    },
+    editProfile: protectedResolver(
+      resolverFn
+    )// (root, args, context, info)
   },
 };
 
